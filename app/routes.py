646f9bb37eefcaa5665a11ecc5b05abc618686ad
Ethan <ethan@Ethan-air-tags-dad.local>
2023-01-10 15:48:04 -0800

    First draft of sorting and I/CC

--- app/routes.py
+++ app/routes.py
@@ -2,32 +2,37 @@ from app import app
 import os
 import requests
 from flask import render_template, url_for, request, redirect, flash, send_file, session
-from flask_paginate import Pagination, get_page_args
 import math
 from PIL import Image
 from collections import defaultdict
 
-# from Image import convert_to_binary
+# TESTING ONLY TODO DELETE THIS LINE OF CODE
+# app.secret_key = os.urandom(32)
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    # app.secret_key = os.urandom(32) # TESTING ONLY TODO DELETE THIS LINE OF CODE
     images = [f'images/{img}' for img in os.listdir('app/static/images')]
-    imageList = defaultdict(list) # key: image_name { value: [intensity, colorCode] }
+    # key: image_name { value: [intensity, colorCode] }
+    imageList = defaultdict(list)
     if not session.get('original_order'):
         original_order = images
-        session['original_order'] = original_order # Sets the original order of all the images
+        # Sets the original order of all the images
+        session['original_order'] = original_order
     # Gets the image list + all of the intensity and Cc values
-    if session.get('imageList'):
+    if not session.get('imageList'):
         print('list not in session')
         for image in os.listdir('app/static/images'):
             imagePath = f'app/static/images/{image}'
             im = Image.open(imagePath)
             CcBin, InBin = encode(im.getdata())
-            imageList[image] = [InBin, CcBin]
-        session['imageList'] = imageList # Incodes values to sessions
+            imageList[image] = {
+                'bins': [InBin, CcBin],  # Get the bins for the image
+                # Get number of pixels in an image
+                'size': len(list(im.getdata()))
+            }
+        session['imageList'] = imageList  # Incodes values to sessions
     else:
         # print(session['imageList'])
         ...
@@ -56,9 +61,16 @@ def get_intensity(filename):
     """
     # print(session.get('imageList'))
     # print(f'will this work? image {filename}', session.get('imageList')[filename.split('?')[1]])
-    targetIntensity = session.get('imageList')[filename.split('?')[1]][0]
-    print(filename, targetIntensity)
+    target_image = filename.split('?')[1]  # Target Image fully parsed from URL
     sortedList = []
+    # TODO Use Manhatten distance to sort the images based on Intensity
+    for image in session.get('imageList').keys():
+        get_distance = manhattanDistance(target_image, image, 'CC')
+        # Append the image and the distance of the image compared to the target_image
+        sortedList.append((get_distance, image))
+    sortedList = sorted(sortedList, key=lambda x: x[0])
+    print(sortedList)
+    session['sortedList'] = sortedList
 
     return redirect(url_for('results', filename=filename))
 
@@ -75,8 +87,18 @@ def get_color_code(filename):
     Args:
         filename (_type_): Filename of the image
     """
-    targetColorCode = session.get('imageList')[filename.split('?')[1]][1]
-    print(filename, targetColorCode)
+    target_image = filename.split('?')[1]  # Target Image fully parsed from URL
+    # print(filename, targetColorCode)
+    sortedList = []
+    # TODO Use Manhatten distance to sort the images based on CC
+    for image in session.get('imageList').keys():
+        print(image)
+        get_distance = manhattanDistance(target_image, image, 'I')
+        # Append the image and the distance of the image compared to the target_image
+        sortedList.append((get_distance, image))
+    sortedList = sorted(sortedList, key=lambda x: x[0])
+    print(sortedList)
+    session['sortedList'] = sortedList
     return redirect(url_for('results', filename=filename))
 
 
@@ -91,11 +113,15 @@ def results(filename):
     Returns:
         _type_: The template that orders the images
     """
-    print('this was "sorted"!')
-    print(filename)
+    # print('this was "sorted"!')
+    # print(filename)
     filename = filename.replace('?', '/')
-    print(session.get('original_order'))
-    return render_template('sorted.html', images_ordered=[])
+    # print(session.get('original_order'))
+    if session.get('sortedList') == []:
+        return redirect(url_for('index'))
+    final_list = session.get('sortedList')
+    final_list = [f'images/{img[1]}' for img in final_list]
+    return render_template('sorted.html', images=final_list)
 
 
 def encode(pixList):
@@ -106,20 +132,58 @@ def encode(pixList):
     # print(pixList[0])
     for pix in pixList:
         # Intensity
-        intensity = (0.2999*pix[0]) + (0.587*pix[1]) + (0.114*pix[2]) # Intensity = 0.2999R + 0.587G + 0.114B
+        # Intensity = 0.2999R + 0.587G + 0.114B
+        intensity = (0.2999*pix[0]) + (0.587*pix[1]) + (0.114*pix[2])
         # print(f'{pix}: {intensity}')
         # print(intensity, int((intensity // 10) % 25) + 1)
-        InBins[int((intensity // 10) % 25)] += 1 # Increments bin[int((intensity // 10) % 25)] inside of the bin
+        # Increments bin[int((intensity // 10) % 25)] inside of the bin
+        InBins[int((intensity // 10) % 25)] += 1
 
         # Color code
         # print(_convert_to_binary(pix[0]), _convert_to_binary(pix[1]), _convert_to_binary(pix[2]))
-        six_digit_code = str(_convert_to_binary(pix[0]))[:2] + str(_convert_to_binary(pix[1]))[:2] + str(_convert_to_binary(pix[2]))[:2]
+        six_digit_code = str(_convert_to_binary(pix[0]))[
+            :2] + str(_convert_to_binary(pix[1]))[:2] + str(_convert_to_binary(pix[2]))[:2]
         # print(int(six_digit_code, 2))
+        # Convert binary to decimal, then increment the bin based on the decimal
         CcBins[int(six_digit_code, 2)] += 1
 
     # print(InBins)
     # print(CcBins)
     return CcBins, InBins
+
+
+def manhattanDistance(target, image, type):
+    """
+    Calculates the manhattan distance between the target and the image
+
+    Args:
+        target (_type_): The target image filename
+        image (_type_): The image to be compared to the target filename
+
+    Returns:
+        _type_: The manhattan distance between the two images
+    """
+    # TODO Calculate the manhattan distance between the target and the image
+    # handles Intensity values
+    distance = 0
+    target_size = session.get('imageList')[target]['size']
+    image_size = session.get('imageList')[image]['size']
+    if type == 'I':
+        target_bin = session.get('imageList')[target]['bins'][0]
+        image_bin = session.get('imageList')[image]['bins'][0]
+        # Loop through the different bins of both the target and image that is being compared
+        for i in range(25):
+            distance += abs(((target_bin[i]) / (target_size)
+                             ) - (image_bin[i] / (image_size)))
+    elif type == 'CC':
+        target_bin = session.get('imageList')[target]['bins'][1]
+        image_bin = session.get('imageList')[image]['bins'][1]
+        for i in range(64):
+            distance += abs(((target_bin[i]) / (target_size)
+                             ) - (image_bin[i] / (image_size)))
+    else:
+        return -1
+    return distance
 
 
 # Helper Functions for simplicity
